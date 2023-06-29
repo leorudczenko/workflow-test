218a0883db531b3da67b89eb34a849cd4c13e9d2
leorud <leorud@bgs.ac.uk>
2023-06-29 10:59:12 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.label_hide = True
-        self.default_message = "Halloo World!"
+        self.default_message = "Hallo World!"
         self.setWindowTitle("Hallo World")
         self.setup_widgets()
 
